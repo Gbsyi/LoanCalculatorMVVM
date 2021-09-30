71cb38ea8b724f9cff144d3b6df4cab67d984b02
Gbsyi <gbsyi@yandex.ru>
2021-09-30 23:02:39 +0700

    Добавьте файлы проекта.

---
+++ LoanCalculator/Models/DiffLoanModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanCalculator.Models
+{
+    class DiffLoanModel
+    {
+        private double _overpayment;
+        public List<double> Payments = new List<double>();
+        private double _totalPayment = 0;
+        public double MainPayment { get; private set; }
+        public double GetOverpayment()
+        {
+            return _overpayment;
+        }
+        public double GetTotalPayment()
+        {
+            return _totalPayment;
+        }
+        public void CalculateLoan(double loan, double interestRate, int months, bool isInterestPerYear)
+        {
+            MainPayment = loan / months;
+            interestRate /= 100;
+
+            if (isInterestPerYear) { interestRate /= 12; }
+
+            for (int index = 0; index < months; index++)
+            {
+                Payments.Add(Math.Round(MainPayment + (loan - (MainPayment * index)) * interestRate, 2, MidpointRounding.ToEven));
+                _totalPayment += Payments[index];
+            }
+
+            _overpayment = Math.Round(_totalPayment - loan, 2, MidpointRounding.ToEven);
+        }
+    }
+}
---
+++ LoanCalculator/ViewModels/LoanViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoanCalculator.Models;
+
+namespace LoanCalculator.ViewModels
+{
+    class LoanViewModel : INotifyPropertyChanged
+    {
+        private DiffLoanModel LoanModel = new();
+        //Инициализация Amount
+        private double _amount;
+        public double Amount
+        {
+            get { return _amount; }
+            set
+            {
+                _amount = value;
+                OnPropertyChanged("Amount");
+            }
+        }
+
+        //Инициализация InterestRate
+        private double _interestRate;
+        public double InterestRate
+        {
+            get { return _interestRate; }
+            set
+            {
+                _interestRate = value;
+                OnPropertyChanged("InterestRate");
+            }
+        }
+
+        //Инициализация Months
+        private int _months;
+        public int Months
+        {
+            get { return _months; }
+            set
+            {
+                _months = value;
+                OnPropertyChanged("Months");
+            }
+        }
+
+        //Инициализация IsInterestPerYear
+        private bool _isInterestPerYear;
+        public bool IsInterestPerYear
+        {
+            get { return _isInterestPerYear; }
+            set
+            {
+                _isInterestPerYear = value;
+                OnPropertyChanged("IsInterestPerYear");
+            }
+        }
+
+        //Получаем список из модели
+        public List<double> GetPayments
+        {
+            get{ return LoanModel.Payments; }
+        }
+
+        //Реализация интерфейса
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
